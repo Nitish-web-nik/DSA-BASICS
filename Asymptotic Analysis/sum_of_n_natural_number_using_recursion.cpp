ab43745daa224673a5791e1b3b569b513e552de6
nitish_web_nik <57797801+Nitish-web-nik@users.noreply.github.com>
2021-01-05 20:57:14 +0530

    Add files via upload

---
+++ Asymptotic Analysis/sum_of_n_natural_number_using_recursion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int recursion(int n)
+{
+    if(n!=0)
+    {
+        return n + recursion(n-1);
+    }
+    else
+    {
+        return n;
+    }
+}
+int main()
+{
+    int n;
+    cout<<"Enter the value of n"<<endl;
+    cin>>n;
+    cout<<recursion(n);
+    return 0;
+}
+
