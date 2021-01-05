90e883e27af3d1587e632320b2f6dc787b902d7b
nitish_web_nik <57797801+Nitish-web-nik@users.noreply.github.com>
2021-01-05 21:00:44 +0530

    Add files via upload

---
+++ Asymptotic Analysis/calculate_power_of_the_base.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Source {
+    // function to calculate base^exponent
+    public static int powerfunc(int base, int power) {
+        //write your code here
+        if(power!=0)
+        {
+
+            return (base* powerfunc(base,power-1));
+        }
+        else
+        {
+            return 1;
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner inpt = new Scanner(System.in);
+        int a = inpt.nextInt();
+        int b = inpt.nextInt();
+        // printing the value of a^b i.e.,
+        System.out.printf("%d", powerfunc(a, b));
+    }
+
+}
