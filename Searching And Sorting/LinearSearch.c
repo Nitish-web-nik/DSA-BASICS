5c91b79ffe7b093861bef69c9269e2ad9b0fe1b9
nitish_web_nik <57797801+Nitish-web-nik@users.noreply.github.com>
2021-01-09 15:46:16 +0530

    Add files via upload

---
+++ Searching And Sorting/LinearSearch.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int arr[6],data,i;
+    printf("Enter 6 Elements:- \n");
+    for(i=0; i<6; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the data to search in the array ");
+    scanf("%d", &data);
+    for(i= 0; i<6;i++)
+    {
+        if(arr[i]== data)
+        {
+            printf("Element found at index %d ",i);
+            break;
+        }
+    }
+    if(i==6)
+    {
+        printf("Element not found in the array");
+    }
+    return 0;
+}
