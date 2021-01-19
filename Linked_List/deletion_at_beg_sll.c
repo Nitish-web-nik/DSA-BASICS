00148cb3ebf9658d770167a90e26db001cc1e1d3
nitish_web_nik <57797801+Nitish-web-nik@users.noreply.github.com>
2021-01-19 22:24:41 +0530

    Add files via upload

---
+++ Linked_List/deletion_at_beg_sll.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    struct node
+    {
+        int data;
+        struct node* next;
+    }*head, *temp, *newnode, *ll;//We will use ll to free the first node
+    int choice = 1;
+    head = 0;
+    while(choice)
+    {
+        newnode = (struct node *)malloc(sizeof(struct node));
+        printf("Enter the data :- ");
+        scanf("%d", &newnode->data);
+        newnode->next = 0;
+        if(head == 0)
+        {
+            head = temp = newnode;
+        }
+        else
+        {
+            temp->next = newnode;
+            temp = newnode;
+        }
+        printf("Do you want to continue?(0,1) ");
+        scanf("%d", &choice);
+    }
+    temp = head;
+    printf("\nCurrent Linked list is :- ");
+    while(temp!= 0)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\nAfter Deletion of First element the Linked list is:- ");
+    ll = head;
+    head = head->next;
+    temp = head;
+    free(ll);
+    while(temp!= 0)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
